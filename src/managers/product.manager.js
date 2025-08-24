5c27520bf6759b8ec3cd556e8e1b3dee5c0e513f
Valentín <valentin.lapalma25@gmail.com>
2025-08-24 16:44:46 -0300

    comentario

--- src/managers/product.manager.js
+++ src/managers/product.manager.js
@@ -31,12 +31,12 @@ const productsTiendaNube = async () => {
         const productsCollection = collection(db, "products");
 
         for (const product of products) {
-            const provinciasValidas = ["Buenos Aires", "Capital Federal", "Ciudad de Buenos Aires"];
-            // Si la provincia no está en la lista, se descarta
-            if (!provinciasValidas.includes(product?.billing_province)) {
-              console.log(`Pedido ${product.id} descartado: Provincia ${product?.billing_province}`);
-              continue;
-            }
+            // const provinciasValidas = ["Buenos Aires", "Capital Federal", "Ciudad de Buenos Aires"];
+            // // Si la provincia no está en la lista, se descarta
+            // if (!provinciasValidas.includes(product?.billing_province)) {
+            //   console.log(`Pedido ${product.id} descartado: Provincia ${product?.billing_province}`);
+            //   continue;
+            // }
 
             // Filtramos pedidos que son retiro en local
             // const esRetiroEnLocal =

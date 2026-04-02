938e16df4fc6d63935c988c95894b91fe224e652
Valentín <valentin.lapalma25@gmail.com>
2026-04-02 14:52:19 -0300

    correcion en sendEmail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,14 +4,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
   tls: {
-    rejectUnauthorized: false
+    minVersion: "TLSv1.2"
   }
 });
 

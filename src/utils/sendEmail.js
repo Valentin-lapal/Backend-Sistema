69c6f4f75435f9b39e32ed5ff545a96a59994a06
Valentín <valentin.lapalma25@gmail.com>
2026-04-02 15:57:29 -0300

    más correcciones

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -10,9 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: {
-    minVersion: "TLSv1.2"
-  }
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 

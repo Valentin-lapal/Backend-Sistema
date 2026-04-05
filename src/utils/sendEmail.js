b2c65e956ea6360292202456d40e762300ac5a05
Valentín <valentin.lapalma25@gmail.com>
2026-04-04 21:38:21 -0300

    agregue resend

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,22 @@
 
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.hostinger.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  family: 4,
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.hostinger.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
+//   family: 4,
+// });
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendTrackingEmail = async (to, trackingId, estado, storeName) => {
 
@@ -25,16 +27,16 @@ const sendTrackingEmail = async (to, trackingId, estado, storeName) => {
 
   if (estado === "EN_CAMINO") {
     titulo = "Tu envío está en camino 🚚";
-    mensaje = `Hola😊 Te escribimos desde Líverval, empresa de logística para ecommerce.\nNos complace informarte que tu pedido de ${storeName || "tu tienda"} ya está en camino y lo vas a estar recibiendo en tu domicilio hoy entre las 12:00 y 20:00 horas.\n\nNúmero de seguimiento: ${trackingId}\n\n¡Gracias por tu compra!😊`;
+    mensaje = `Hola😊 Te escribimos desde Líverval, empresa de logística para ecommerce.<br>Nos complace informarte que tu pedido de ${storeName || "tu tienda"} ya está en camino y lo vas a estar recibiendo en tu domicilio hoy entre las 12:00 y 20:00 horas.<br><br>Número de seguimiento: ${trackingId}<br><br>¡Gracias por tu compra!😊`;
   }
 
   if (estado === "ENTREGADO") {
     titulo = "Tu envío fue entregado 📦";
-    mensaje = `Tu pedido de ${storeName} fue entregado correctamente.\n\nEsperamos que disfrutes mucho tu compra😊`;
+    mensaje = `Tu pedido de ${storeName} fue entregado correctamente.<br><br>Esperamos que disfrutes mucho tu compra😊`;
   }
 
-  await transporter.sendMail({
-    from: `"Líverval Logística" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "Líverval Logística <onboarding@resend.dev>",
     to,
     subject: titulo,
     html: `
